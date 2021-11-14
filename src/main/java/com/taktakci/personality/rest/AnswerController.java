69c3f37f6f79534bba06f9557468ef7903e998da
Tarik Taktakci <ttaktakci@gmail.com>
2021-11-14 10:11:38 +0300

    CORS allowed

--- src/main/java/com/taktakci/personality/rest/AnswerController.java
+++ src/main/java/com/taktakci/personality/rest/AnswerController.java
@@ -8,6 +8,7 @@ import com.taktakci.personality.service.model.AnswerDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/answer")
 public class AnswerController {
